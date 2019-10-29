71301f1d24d815d8dac464c140d10ce649338310
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-29 22:57:10 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+ class Dog
+  def name=(name)
+      @name = name
+  end
+  def name 
+    @name
+  end 
+  def breed=(new_name)
+    @breed =new_name
+  end 
+  def breed 
+    @breed
+  end 
+end
+ 
+ 
